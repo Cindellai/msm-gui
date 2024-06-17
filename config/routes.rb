c50acb414bed329028b7dd1d31f1a062c33bd572
cindellai <cindellai26@gmail.com>
2024-06-17 10:03:57 -0500

    added movies functionality

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
-  
+  post("/insert_movie_record", { :controller => "movies", :action => "create" })
+  post("/update_movie", { :controller => "movies", :action => "update" })
+  delete("/delete_movie/:id", { :controller => "movies", :action => "destroy" })
+
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
 end
